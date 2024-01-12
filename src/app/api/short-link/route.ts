e9cb2222df2f025bcd0791fa5e5f84dca9cffef5
Damian <damjeronzalez23@gmail.com>
2024-01-12 16:32:08 -0500

    add route get for get info for short link

--- src/app/api/short-link/route.ts
+++ src/app/api/short-link/route.ts
@@ -72,3 +72,70 @@ export async function POST(request: NextRequest, response: NextResponse) {
     })
   }
 }
+
+export async function GET(request: NextRequest, response: NextResponse) {
+  try {
+    const searchParams = request.nextUrl.searchParams
+    const id = searchParams.get("id")
+    if (id == "") {
+      let error_response = {
+        status: "fail",
+        message: "Values empty for save shortLink"
+      }
+      return new NextResponse(JSON.stringify(error_response), {
+        status: 400,
+        headers: { "Content-Type": "application/json" }
+      })
+    }
+    try {
+      const [results] = await (
+        await connection
+      ).query(`SELECT * FROM links WHERE idlink = ?`, [id])
+      const resultsArr = results as Array<Object>
+      if (resultsArr.length == 0) {
+        return new NextResponse(
+          JSON.stringify({
+            status: "fail",
+            message: "Short link with id not exists"
+          }),
+          {
+            status: 400,
+            headers: { "Content-Type": "application/json" }
+          }
+        )
+      }
+
+      return new NextResponse(
+        JSON.stringify({
+          status: "success",
+          shortLinkInfo: resultsArr[0]
+        }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+    } catch (err) {
+      console.log(err)
+      return new NextResponse(
+        JSON.stringify({
+          status: "error",
+          message: err
+        }),
+        {
+          status: 500,
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+    }
+  } catch (error: any) {
+    let error_response = {
+      status: "error",
+      message: error.message
+    }
+    return new NextResponse(JSON.stringify(error_response), {
+      status: 500,
+      headers: { "Content-Type": "application/json" }
+    })
+  }
+}
