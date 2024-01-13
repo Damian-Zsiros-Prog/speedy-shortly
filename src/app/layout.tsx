5603d19b72a748468b3e4d5b9143e75c1bb3b81c
Damian <damjeronzalez23@gmail.com>
2024-01-13 10:02:53 -0500

    add metadata open graph and more metadata in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,20 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Speedy Shortly",
-  description: "Speedy Shortly"
+  description:
+    "Speedy Shortly, tu compañero ágil en la web. Acorta tus enlaces de forma instantánea y sin complicaciones. Experimenta la velocidad sin igual de Speedy Shortly, el acortador que simplifica tus URL de manera rápida y eficiente. ¡Sin registros, solo resultados veloces para compartir tus enlaces de manera ágil y sencilla!",
+  openGraph: {
+    images: [
+      {
+        url: "/og-image.png",
+        alt: "Og Image of Speedy Shortly",
+        type: "image/png",
+        width: 1200,
+        height: 720
+      }
+    ]
+  },
+  keywords: ["shortlink", "speedy shortly", "shortly", "link"]
 }
 
 export default function RootLayout({
